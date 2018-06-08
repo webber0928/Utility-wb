e88d3ec951a78f8813ba2650b7e581c50da55c75
webber.wu <bird23074035@gmail.com>
2018-06-08 16:01:05 +0800

    update

--- fakeJson/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  default: require('./default.json'),
-  Talk_to_big_wifi: require('./Talk_to_big_wifi.json'),
-  help: require('./help.json'),
-  enable_my_guest_zone: require('./enable_my_guest_zone.json'),
-  "enable_my_guest_zone_on_dir-2680": require('./enable_my_guest_zone_on_dir-2680'),
-  enable_my_guest_zone_NO: require('./enable_my_guest_zone_NO'),
-  enable_my_guest_zone_YES: require('./enable_my_guest_zone_YES'),
-};
